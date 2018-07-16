ac4bae4ea398937b68f2a75f10461d310605d46d
Sarah Schaller <sarahschaller@Sarahs-MacBook-Pro.local>
2018-07-16 19:24:54 -0400

    spelling error

--- db/migrate/20180716225943_create_student_classrooms.rb
+++ db/migrate/20180716225943_create_student_classrooms.rb
@@ -2,7 +2,7 @@ class CreateStudentClassrooms < ActiveRecord::Migration[5.2]
   def change
     create_table :student_classrooms do |t|
     	t.integer :student_id
-    	t.intger :classroom_id
+    	t.integer :classroom_id
 
       t.timestamps
     end
