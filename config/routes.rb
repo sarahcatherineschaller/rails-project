b3a4a7ebea954ec26da801079dd8a35a5b9f369b
Sarah Schaller <sarahschaller@Sarahs-MacBook-Pro.local>
2018-07-16 19:59:45 -0400

    add session paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
 	root 'users#index'
 	get '/users', to: 'users#index', as: 'users'
 	get '/users/new', to: 'users#new', as: 'new_user'
-	get 'login', to: 'session#new', as: 'login'
 	post '/users', to: 'users#create'
 	get '/users/:id', to: 'users#show', as: 'user'
+
+	get '/login', to: 'session#new', as: 'login'
+	post '/session', to: 'session#create', as: 'session'
+	delete '/session', to: 'session#destroy'
 
 end
 
