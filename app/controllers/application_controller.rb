6fb4945d99cf72585513ffc3ad5d9e5d9c5e84fe
Sarah Schaller <sarahschaller@Sarahs-MacBook-Pro.local>
2018-07-16 19:33:29 -0400

    add current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	helper_method :current_user
+
+	private
+	def current_user 
+		User.find_by(id:sessions[:user_id])
+	end
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,6 @@
 class SessionController < ApplicationController
+	def destroy
+		session.delete("user_id")
+		redirect_to root_path
+	end
 end
