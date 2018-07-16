4581f72ccc2cff853b5ab428f55e7d76eacd7bad
Sarah Schaller <sarahschaller@Sarahs-MacBook-Pro.local>
2018-07-16 19:20:24 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 	root 'users#index'
+	get '/users', to: 'users#index', as: 'users'
+	get '/users/new', to: 'users#new', as: 'new_user'
+	get 'login', to: 'session#new', as: 'login'
+	
+
 end
 
