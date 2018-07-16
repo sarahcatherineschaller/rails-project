6425fa38873f24c795c33061e5a67b991ab14e47
Sarah Schaller <sarahschaller@Sarahs-MacBook-Pro.local>
2018-07-16 19:03:28 -0400

    create tables

--- db/migrate/20180716225922_create_classrooms.rb
+++ db/migrate/20180716225922_create_classrooms.rb
@@ -1,6 +1,8 @@
 class CreateClassrooms < ActiveRecord::Migration[5.2]
   def change
     create_table :classrooms do |t|
+    	t.integer :user_id
+    	t.string :subject
 
       t.timestamps
     end
--- db/migrate/20180716225943_create_student_classrooms.rb
+++ db/migrate/20180716225943_create_student_classrooms.rb
@@ -1,6 +1,8 @@
 class CreateStudentClassrooms < ActiveRecord::Migration[5.2]
   def change
     create_table :student_classrooms do |t|
+    	t.integer :student_id
+    	t.intger :classroom_id
 
       t.timestamps
     end
