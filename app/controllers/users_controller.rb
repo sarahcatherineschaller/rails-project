24d8c0a62f98abe611c27e202f588137b015ef2d
Sarah Schaller <sarahschaller@Sarahs-MacBook-Pro.local>
2018-07-16 19:41:55 -0400

    post to create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,22 @@ class UsersController < ApplicationController
 		@user = User.new 
 	end
 
-	
+	def create 
+		if (user = User.create user_params)
+			session[:user_id] = user.id 
+			redirect_to user_path(user)
+		else 
+			render 'new'
+		end 
+	end 
+
+	def show 
+	end 
+
+	private 
+	def user_params
+		params.require(:user).permit(:title, :first_name, :last_name, :email, :password)
+	end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 	get '/users', to: 'users#index', as: 'users'
 	get '/users/new', to: 'users#new', as: 'new_user'
 	get 'login', to: 'session#new', as: 'login'
-	
+	post '/users', to: 'users#create'
 
 end
 
