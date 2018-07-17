ed49cd33dd7a4b3dc0295d546e2492605e254a91
Sarah Schaller <sarahschaller@Sarahs-MacBook-Pro.local>
2018-07-17 14:36:25 -0400

    new classroom link

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 	post '/session', to: 'session#create', as: 'session'
 	delete '/session', to: 'session#destroy'
 
+	get '/classrooms/new', to: 'classrooms#new', as: 'new_classroom'
 	
 
 end
