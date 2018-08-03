5465a3290289b6a49e51581f3d57bff3ef2ab1b4
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-03 16:34:43 -0400

    add student_params

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,25 @@
 class StudentsController < ApplicationController
+	def index 
+	end 
+
+	def new 
+	end
+
+	def create 
+	end
+
+	def show 
+	end 
+
+	def edit 
+	end 
+
+	def destroy 
+	end
+
+	private
+	def student_params 
+		params.require(:student).permit(:first_name, :last_name, :grade, classroom_ids:[])
+	end 
+
 end
