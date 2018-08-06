588c5345948126ae632c715bb18c8dc8b6567e01
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-06 14:18:51 -0400

    add checkboxes for students to classroom form

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,9 @@ class Student < ApplicationRecord
 	has_many :student_classrooms
 	has_many :classrooms, through: :student_classrooms
 	has_many :users, through: :classrooms
+
+	def full_name
+		"#{first_name} #{last_name}"
+	end
+
 end
