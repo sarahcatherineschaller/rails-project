9eb1e75fda58e9cdcecc859d1e8ce29b9920d9b7
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-07 09:09:47 -0400

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+	validates :title, presence: true
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, presence: true, uniqueness: true
+	validates :password, presence: true
+
+
 	has_secure_password
 	has_many :classrooms
 	has_many :students, through: :classrooms
