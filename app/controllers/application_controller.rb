2c0c16dad2f0f3300cceea4b1308e404efefb53c
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-11 11:58:58 -0400

    sign up with devise using user attrs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
+	before_action :authenticate_user!
 	helper_method :current_user
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected 
+
+	def configure_permitted_parameters 
+		added_attrs = [:title, :first_name, :last_name]
+		devise_parameter_sanitizer.permit :sign_up, keys: added_attrs 
+		devise_parameter_sanitizer.permit :account_upddate, keys: added_attrs 
+	end 
 
 	private
 	def current_user 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_11_005542) do
+ActiveRecord::Schema.define(version: 2018_08_11_005818) do
 
   create_table "classrooms", force: :cascade do |t|
     t.integer "user_id"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2018_08_11_005542) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
